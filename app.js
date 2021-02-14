e8a2299ff34a329885df9e37811ecc262a24adf8
sadie-walker <sadie-walker@users.noreply.github.com>
2021-02-14 16:47:16 +0000

    main menu games sub menu functionality

--- app.js
+++ app.js
@@ -2,29 +2,36 @@
 let body = document.querySelector("body");
 
 // Navbars
+let mainNavGamesMenu = document.getElementById("main-nav-games");
 let hamburgerMenu = document.getElementById("mobile-hamburger-nav");
 let mobileAccMenu = document.getElementById("mobile-account-nav");
 
 // Overlay
-let mobileNavOverlay = document.getElementById("mobile-navbars-overlay");
+let menuBgOverlay = document.getElementById("menu-background-overlay");
 
 // Buttons
+let mainNavGamesBtn = document.getElementById("games-menu-button");
 let hamburgerBtn = document.getElementById("main-hamburger-button");
 let mobileAccBtn = document.getElementById("mobile-account-button");
 let hamburgerCloseBtn = document.getElementById("mobile-hamburger-close-button");
 let mobileAccCloseBtn = document.getElementById("mobile-account-close-button");
 
-
+// Event Listeners
+mainNavGamesBtn.addEventListener("click", openMenu);
 hamburgerBtn.addEventListener("click", openMenu);
 mobileAccBtn.addEventListener("click", openMenu);
 hamburgerCloseBtn.addEventListener("click", closeMenu);
 mobileAccCloseBtn.addEventListener("click", closeMenu);
 
+// Open Menus
 function openMenu(e) {
     body.style.overflow = "hidden";
-    mobileNavOverlay.style.visibility = "visible";
+    menuBgOverlay.style.visibility = "visible";
 
-    if(e.target.id === "main-hamburger-button"){
+    if(e.target.id === "games-menu-button"){
+        mainNavGamesMenu.style.visibility = "visible";
+        e.target.firstElementChild.classList.add("navbar-arrow-rotation");
+    } else if(e.target.id === "main-hamburger-button"){
         hamburgerMenu.style.left = "0";
         mobileAccMenu.style.right = "-35rem";
     } else {
@@ -33,6 +40,7 @@ function openMenu(e) {
     }
 }
 
+// Close Menus
 function closeMenu(e){
     body.style.overflow = "visible";
     mobileNavOverlay.style.visibility = "hidden";
