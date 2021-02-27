64a3fd3b90183003b3d26e51904f9a62d33549d0
Sadie Walker <64597433+sadie-walker@users.noreply.github.com>
2021-02-27 12:57:47 +0000

    games and esports menus functionality

--- app.js
+++ app.js
@@ -2,61 +2,69 @@
 let body = document.querySelector("body");
 
 // Navbars
+let mainNav = document.querySelector(".main-nav");
 let mainNavGamesMenu = document.querySelector(".main-nav-games");
+let mainNavEsportsMenu = document.querySelector(".main-nav-esports");
+let mobileMainNav = document.querySelector(".mobile-main-nav");
 let hamburgerMenu = document.querySelector(".mobile-hamburger-nav");
 let mobileAccMenu = document.querySelector(".mobile-account-nav");
 
 // Overlay
-let menuBgOverlay = document.getElementById("menu-background-overlay");
+let menuBgOverlay = document.querySelector(".background-overlay");
 
 // Buttons
 let mainNavGamesBtn = document.getElementById("games-menu-button");
+let mainNavEsportsBtn = document.getElementById("esports-menu-button");
 let hamburgerBtn = document.getElementById("main-hamburger-button");
 let mobileAccBtn = document.getElementById("mobile-account-button");
 let hamburgerCloseBtn = document.getElementById("mobile-hamburger-close-button");
 let mobileAccCloseBtn = document.getElementById("mobile-account-close-button");
 
 // Event Listeners
-mainNavGamesBtn.addEventListener("click", toggleGamesMenu);
-hamburgerBtn.addEventListener("click", openMenu);
-mobileAccBtn.addEventListener("click", openMenu);
-hamburgerCloseBtn.addEventListener("click", closeMenu);
-mobileAccCloseBtn.addEventListener("click", closeMenu);
+mainNavGamesBtn.addEventListener("click", toggleMainSubMenus);
+mainNavEsportsBtn.addEventListener("click", toggleMainSubMenus);
+hamburgerBtn.addEventListener("click", toggleSideMenu);
+mobileAccBtn.addEventListener("click", toggleSideMenu);
+hamburgerCloseBtn.addEventListener("click", toggleSideMenu);
+mobileAccCloseBtn.addEventListener("click", toggleSideMenu);
 
 // Open Menus
-function openMenu(e) {
-    body.style.overflow = "hidden";
-    menuBgOverlay.style.visibility = "visible";
+function toggleSideMenu(e) {
+    body.style.overflowY = "hidden";
+    menuBgOverlay.classList.add("toggle-visibility");
         
     if(e.target.id === "main-hamburger-button"){
-        hamburgerMenu.style.left = "0";
-        mobileAccMenu.style.right = "-35rem";
+        mobileMainNav.classList.add("nav-overlay");
+        hamburgerMenu.classList.add("hmbg-mobile-menu-open");
+        mobileAccMenu.classList.remove("acc-mobile-menu-open");
+    } else if(e.target.id === "mobile-account-button"){
+        mobileMainNav.classList.add("nav-overlay");
+        mobileAccMenu.classList.add("acc-mobile-menu-open");
+        hamburgerMenu.classList.remove("hmbg-mobile-menu-open"); 
     } else {
-        mobileAccMenu.style.right = "0";
-        hamburgerMenu.style.left = "-35rem";
+        hamburgerMenu.classList.remove("hmbg-mobile-menu-open");
+        mobileAccMenu.classList.remove("acc-mobile-menu-open");
+        mobileMainNav.classList.remove("nav-overlay");
+        menuBgOverlay.classList.remove("toggle-visibility");
+
+        body.style.overflowY = "visible";
     }
 }
 
-// Close Menus
-function closeMenu(e){
-    body.style.overflow = "visible";
-    menuBgOverlay.style.visibility = "hidden";
-    
-    if(e.target.id === "mobile-hamburger-close-button"){
-        hamburgerMenu.style.left = "-35rem";
+function toggleMainSubMenus(e){
+    e.target.firstElementChild.classList.toggle("navbar-arrow-rotation")
+    console.log("WE'RE IN");
+    if(e.target.id === "games-menu-button"){
+        mainNav.classList.toggle("nav-overlay");
+        mainNavGamesMenu.classList.toggle("toggle-visibility");
+        mainNavEsportsMenu.classList.remove("toggle-visibility")
+        mainNavEsportsBtn.firstElementChild.classList.remove("navbar-arrow-rotation");
+    } else if (e.target.id === "esports-menu-button"){
+        console.log("ESPORTS!");
+        mainNavEsportsMenu.classList.toggle("toggle-visibility")
+        mainNavGamesMenu.classList.remove("toggle-visibility");
+        mainNavEsportsBtn.firstElementChild.classList.remove("navbar-arrow-rotation");
     } else {
-        mobileAccMenu.style.right = "-35rem";
+        mainNavAccMenu.classList.toggle("toggle-visibility");
     }
-}
-
-function toggleGamesMenu(){
-    mainNavGamesMenu.classList.toggle("toggle-visibility");
-    // if(mainNavGamesMenu.style.visibility = "hidden"){
-    //     mainNavGamesMenu.style.visibility = "visible";
-    //     e.target.firstElementChild.classList.add("navbar-arrow-rotation");
-    // } else {
-    //     console.log("WORKING");
-    //     mainNavGamesMenu.style.visibility = "hidden";
-    //     e.target.firstElementChild.classList.remove("navbar-arrow-rotation");
-    // }
 }
